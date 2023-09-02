9704ad9d970e5566ff4a94c6962aeeaff05eb518
emy-gaudia <emza.gaudia.up@phinmaed.com>
2023-09-02 11:32:47 +0800

    first commit

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ color, text }) => {
+  return (
+    <>
+        <button className={`button ${color}-button`}>
+        {text}
+        </button>
+    </>
+  );
+};
+
+export default Button;
